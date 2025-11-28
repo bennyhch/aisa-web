54313d39df06ec2e4a6aa8ecd0112abd9f43a299
Ben Ho <bennyhch@gmail.com>
2025-11-28 14:42:57 +0000

    feat: add google metatag for tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          name="google-site-verification"
+          content="55Q7ZOM5WkeUqtVI9ywUCIgggHS4xbvy_7NVdwmn128"
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
