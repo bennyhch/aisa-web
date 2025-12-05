81e70458de51fbb05b14a0176f958043ca35a9d2
Supheakmungkol Sarin <mungkol@gmail.com>
2025-12-04 23:09:15 -0800

    Update founders.ts
    Remove Lyan as she left the org.

--- src/data/founders.ts
+++ src/data/founders.ts
@@ -1,13 +1,13 @@
 const founders = [
   {
     id: 1,
-    name: "LYANTONIETTE CHUA",
+    name: "DR. SUPHEAKMUNGKOL SARIN",
     title: "Co-Founder",
-    image: "/team/Lyan-Chua.png",
+    image: "/team/Mungkol.png",
     bulletPoints: [
-      "Senior Policy Advisor and Fellow at the Center for AI and Digital Policy, Washington DC, and founder of The Ambit.",
-      "Recognized leader in AI governance and listed among 100 Brilliant Women in AI Ethics for 2024.",
-      "Founding member of the UN University Macau AI Network and OECD.AI Experts Group on AI, Data, and Privacy.",
+      "Global Tech & Policy Advisor advising the UN and World Bank on GovTech and national AI strategy, backed by 20 years of data and AI leadership.",
+      "As Head of Data & AI Ecosystems at the World Economic Forum, helped launch the AI Governance Alliance, co-founded the Safe Systems & Technologies Initiative, and convened the inaugural AI Governance Summit (2023).",
+      "At Google Research, directed inclusive-AI programs that expanded responsible AI tools, datasets and products to more than one billion users worldwide."
     ],
   },
   {
@@ -19,17 +19,6 @@ const founders = [
       "Founder of Connecting Myanmar and experienced leader in technology startups and non-profits.",
       "Led the APAC business of a late-stage startup with over $100M raised.",
       "Former board member of Amnesty International Hong Kong and advisor to multiple corporate-NGO initiatives.",
-    ],
-  },
-  {
-    id: 3,
-    name: "DR. SUPHEAKMUNGKOL SARIN",
-    title: "Executive Director",
-    image: "/team/Mungkol.png",
-    bulletPoints: [
-      "AI Expert to the UN Secretary-General’s High-Level Advisory Body on AI and Senior Strategy Consultant to UNESCAP and the World Bank, backed by 20 years of data and AI leadership.",
-      "As Head of Data & AI Ecosystems at the World Economic Forum, helped launch the AI Governance Alliance, co-founded the Safe Systems & Technologies Initiative, and convened the inaugural AI Governance Summit (2023).",
-      "At Google Research, directed inclusive-AI programs that expanded responsible AI tools, datasets and products to more than one billion users worldwide."
     ],
   },
 ];
